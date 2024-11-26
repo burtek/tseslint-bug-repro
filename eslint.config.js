2a2b12edc9b86d12c4c4d96b89154016a86f6328
Bartosz <burtekdotryw@gmail.com>
2024-11-26 08:44:09 +0100

    Minimize repro

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,15 @@
-/* eslint-disable */
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
     tseslint.configs.recommended,
-    tseslint.configs.recommendedTypeChecked,
     {
         languageOptions: {
             parserOptions: {
                 project: 'tsconfig.json'
             },
         }
+    },
+    {
+        ignores: ["eslint.config.js"]
     }
 )
